17eecb937d89f49438faae66aed3981bf7dc4e6f
D280518 <39691357+D280518@users.noreply.github.com>
2018-05-28 16:32:29 +0200

    Test Pipeline
    Test Pipeline

--- build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.devopscon.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hi Devopscon? What do you think about this WAR file?";
+		String hello = "Hi Pipeline? What do you think about this WAR file?";
 		return hello;
 	}
 
